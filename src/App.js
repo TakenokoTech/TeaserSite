e83eaa1c46bd57fe4af54769d12b6b8bbb394461
Takenoko <takenoko.tech@gmail.com>
2018-08-12 02:14:37 +0900

    fix image.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import './css/SubTitle1.css';
 
 import sampleImage1 from './img/sample1.png'
 import sampleImage2 from './img/sample2.png'
+import sampleImage3 from './img/c1.png'
+import sampleImage4 from './img/c2.png'
+import sampleImage5 from './img/c3.png'
 
 // import Three from './Three'
 
@@ -18,25 +21,22 @@ class App extends Component {
     super(props)
     this.state = {
       showModal: false,
+      showNumber: 0,
       image: [
-        sampleImage1, sampleImage2,
-        sampleImage1, sampleImage2,
-        sampleImage1, sampleImage2,
-        sampleImage1, sampleImage2,
-        sampleImage1, sampleImage2,
-        sampleImage1, sampleImage2,
-        sampleImage1, sampleImage2,
-        sampleImage1, sampleImage2,
-        sampleImage1, sampleImage2,
-        sampleImage1, sampleImage2,
+        sampleImage1, sampleImage2,sampleImage3, sampleImage4, sampleImage5,
+        sampleImage1, sampleImage2,sampleImage3, sampleImage4, sampleImage5,
+        sampleImage1, sampleImage2,sampleImage3, sampleImage4, sampleImage5,
       ]
     }
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
   }
 
-  handleOpenModal () {
-    this.setState({ showModal: true });
+  componentDidMount() {
+  }
+
+  handleOpenModal (no = 0) {
+    this.setState({ showModal: true, showNumber: no });
   }
   
   handleCloseModal () {
@@ -48,17 +48,16 @@ class App extends Component {
       <Fragment>
         <div id="overview">
           <div id="character_dialog">
-            <div id="chara_name">このの</div>
+            <div id="chara_name">このの<span> (16)</span></div>
             <div id="chara_grade">すっこすこ高等学校2年</div>
             <div id="chara_desc">
-            この春から仕事を始めることになった魔法戦士。<br/>
             いわゆるツンデレであり本心を表に出すのは苦手だが、<br/>
             実は子供好きであり自身も子供から好かれている。<br/>
             ゴシック調のコーデを好む。<br/>
             </div>
           </div>
         </div>
-        <div className="subtitle">
+        <div className="subtitle z-depth-3">
           <div className="size_title">サブタイトル</div>
           <div className="size_overview">
             hogehogehogehogehogehogehogehogehogehogehogehogehogehoge<br/>
@@ -66,9 +65,9 @@ class App extends Component {
           </div>
         </div>
         <div id="overview">
-          { _.map(this.state.image, (img, i) => <img key={i} src={img} className="waves-effect z-depth-2 image_cell" onClick={this.handleOpenModal} />) }
+          { _.map(this.state.image, (img, i) => <img key={i} src={img} className="waves-effect z-depth-2 image_cell" onClick={() => this.handleOpenModal(i)} />) }
         </div>
-        <div className="subtitle">
+        <div className="subtitle z-depth-3">
           <div className="size_title">サブタイトル</div>
           <div className="size_overview">
             hogehogehogehogehogehogehogehogehogehogehogehogehogehoge<br/>
@@ -76,7 +75,7 @@ class App extends Component {
           </div>
         </div>
         <div id="overview">
-          { _.map(this.state.image, (img, i) => <img key={i} src={img} className="waves-effect z-depth-2 image_cell" onClick={this.handleOpenModal} />) }
+          { _.map(this.state.image, (img, i) => <img key={i} src={img} className="waves-effect z-depth-2 image_cell" onClick={() => this.handleOpenModal(i)} />) }
         </div>
         
         <Modal
@@ -85,7 +84,7 @@ class App extends Component {
           style={{
             overlay: { backgroundColor: '#000000CC' },
             content: { backgroundColor: '#F4F4F4' }}}>
-          <img src={sampleImage1} />
+          <img src={this.state.image[this.state.showNumber]} />
           <a class="waves-effect waves-light teal-text" onClick={this.handleCloseModal}><i class="material-icons">clear</i></a>
         </Modal>
       </Fragment>
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import konono_logo from './img/konono_logo_c.png';
+import official from './img/official_w.png';
+import logo from './img/logo.svg';
 import './css/App.css';
 
 // import Three from './Three'
@@ -8,9 +10,10 @@ class Header extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header base_color z-depth-3">
+          <img src={konono_logo} className="App-logo" alt="logo" />
+          <img src={official} className="App-title" alt="logo" />
+          {/* <h1 className="App-title accent_color">Official Website</h1> */}
         </header>
         {/* <Three /> */}
       </div>
